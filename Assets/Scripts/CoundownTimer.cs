4430a5b0e9f297539a7cb51d0e8e2c89536d34c5
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2023-04-12 23:32:12 -0400

    Countdown timer

---
+++ Assets/Scripts/CoundownTimer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoundownTimer : MonoBehaviour
+{
+    [SerializeField] private float durationInSeconds;
+    [SerializeField] private Text timerLabel;
+
+    private float timeLeft;
+
+    void Start()
+    {
+        timeLeft = durationInSeconds;
+    }
+
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            timerLabel.text = $"{Mathf.FloorToInt(timeLeft / 60):0}:{Mathf.FloorToInt(timeLeft % 60):00}";
+        }
+        else if (timeLeft < 0)
+        {
+            timeLeft = 0;
+            timerLabel.text = "0:00";
+        }
+    }
+}
